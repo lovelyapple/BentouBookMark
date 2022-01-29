a063bafa0a123cbf53dd9ea45f09f6567dde0f6b
lovelyapple <lovelyappleoffice@gmail.com>
2022-01-29 16:11:33 +0900

    1st commit

---
+++ Assets/Scripts/UniRxTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+public class UniRxTest : MonoBehaviour
+{
+    Subject<string> sub = null;
+    void Start()
+    {
+        sub = new Subject<string>();
+        sub.Subscribe(t => Debug.Log(t));
+    }
+    void Update()
+    {
+        sub.OnNext("hoge hoge ");
+    }
+}
