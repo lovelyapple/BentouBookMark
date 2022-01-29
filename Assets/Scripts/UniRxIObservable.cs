ff8ef3ef6d22f5bd081466b7f4e290f73250b184
lovelyapple <lovelyappleoffice@gmail.com>
2022-01-29 17:00:17 +0900

    2.IObservable

---
+++ Assets/Scripts/UniRxIObservable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class UniRxIObservable : MonoBehaviour
+{
+    Subject<string> subject = null;
+    IObservable<string> Observable { get { return subject; } }
+    void Start()
+    {
+        subject = new Subject<string>();
+        Observable.Subscribe(t => Debug.Log(t));
+    }
+
+    void Update()
+    {
+        subject.OnNext("hoge hoge");
+    }
+}
