b85cad335a795c25ac11085631ab31da9aa4274e
lovelyapple <lovelyappleoffice@gmail.com>
2022-01-29 17:38:36 +0900

    5. Operator

---
+++ Assets/Scripts/UniRxOperator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class UniRxOperator : MonoBehaviour
+{
+    Subject<int> subject = null;
+    IObservable<int> Observable { get { return subject; } }
+    public int currentValue = -10;
+    void Start()
+    {
+        subject = new Subject<int>();
+        Observable.Where(v => v <= 10 && 1/v < 10)
+                    .Select(x => x + 1)
+                    .Subscribe(y => Debug.Log("filtering result " + y),
+                    e => Debug.Log("error " + e));
+    }
+
+    void Update()
+    {
+        subject.OnNext(currentValue);
+        currentValue++;
+    }
+}
