f5f3951dc93d28597b8de21f22627f42bbc9147b
lovelyapple <lovelyappleoffice@gmail.com>
2022-01-30 16:20:08 +0900

    7. ReactiveProperties

---
+++ Assets/Scripts/UniRx/UniRxReactiveProperties.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+public class UniRxReactiveProperties : MonoBehaviour
+{
+    ReactiveProperty<uint> valueReactiveProperty = new ReactiveProperty<uint>(0);
+    IObservable<uint> valueObservable { get { return valueReactiveProperty; } }
+    void Start()
+    {
+        valueObservable.Subscribe(v => Debug.Log("value " + v), e => Debug.Log("error " + e));
+
+        for (uint i = 0; i < 10; i++)
+            valueReactiveProperty.Value = i;
+
+        valueReactiveProperty.Value = 10;// wont active
+    }
+}
