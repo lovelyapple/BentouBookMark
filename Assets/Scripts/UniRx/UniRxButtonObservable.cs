7653010b4a05f0f11403b443baae699bdb459209
lovelyapple <lovelyappleoffice@gmail.com>
2022-01-30 15:53:31 +0900

    6.Button On RuniRx

---
+++ Assets/Scripts/UniRx/UniRxButtonObservable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+public class UniRxButtonObservable : MonoBehaviour
+{
+    [SerializeField] Button button;
+
+    void Start()
+    {
+        button.OnClickAsObservable().Buffer(2).Subscribe(msg => Debug.Log("hited "));
+    }
+}
